f33b47b4a0a98479ca3675de26efa5c40c68bb1c
陈少桐 <1528841402@qq.com>
2020-06-30 18:37:02 +0800

    3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
-
+import { Button } from 'antd';
+import './App.css'
 export default class App extends Component {
+
+    
     render() {
         return (
             <div>
-                App...
+                App...123<br/>
+                <Button type="primary">Button</Button>
             </div>
         )
     }
