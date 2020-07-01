1adc2b8b1029583a10a22cd9c50e8d207cef8f11
陈少桐 <1528841402@qq.com>
2020-07-01 14:12:28 +0800

    4

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
-
+import './login.less'
 export default class Login extends Component {
     render() {
         return (
-            <div>
-                Login
+            <div className="login">
+                <header className="login-header"></header>
+                <section className = "login-header"></section>
             </div>
         )
     }
