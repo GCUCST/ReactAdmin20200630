7c0d7970c5b9bc170ffb6750b0cfcafe73e0e339
陈少桐 <1528841402@qq.com>
2020-07-02 11:23:56 +0800

    abc

---
+++ src/api/ajax.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+// import {message} from 'antd'
+
+
+export default function ajax(url,data={},method='GET'){
+        if(method ==='GET'){
+            return  axios.get(url,{params:data})
+        }else{
+            return  axios.post(url,data)
+        }
+
+}
+
+// export default function ajax(url,data={},method='GET'){
+//     return new Promise(function(resolve,reject){
+//         let promise
+//         if(method ==='GET'){
+//             promise = axios.get(url,{params:data})
+//         }else{
+//             promise = axios.post(url,data)
+//         }
+//         promise.then(response=>{
+//             resolve(response.data)
+//         }).catch(error=>{
+//             message.error('请求错误'+error.message)
+//         })
+
+//     })
+// }
